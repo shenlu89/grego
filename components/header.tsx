9a7302225afd59236a8586d67e7aa6ffcb81236e
shenlu89 <shenlu89dev@gmail.com>
2024-08-09 01:56:41 +0800

    chore: change logo

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,6 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { PiPlantDuotone } from "react-icons/pi";
 import { HeaderNavLinks } from "@/data/meta-data";
 
 const Navbar: NextPage = () => {
@@ -12,7 +11,7 @@ const Navbar: NextPage = () => {
     <header className="flex max-w-6xl container mx-auto select-none md:relative fixed inset-x-0 bottom-0 z-50">
       <div className="flex justify-between w-full mx-4 py-4 border border-x-0 border-t-0 border-b-slate-200">
         <Link href={"/"} className="flex text-green-500 space-x-1 items-center">
-          <PiPlantDuotone className="flex size-6" />
+          <img src="/logo.svg" className="flex size-8" />
           <span className="text-xl font-extrabold tracking-tighter self-center pr-8">
             GRE Tools
           </span>
