073888dc6324c7e887207bbd0ae8e57947843ed1
shenlu89 <shenlu89dev@gmail.com>
2025-05-02 09:32:53 +0800

    chore: remove unused files

--- lib/utils.ts
+++
@@ -1,6 +0,0 @@
-export const encodeBase64 = async (file: Blob) => {
-  const fileReader = new FileReader();
-  fileReader.readAsDataURL(file);
-  await new Promise<void>((resolve) => (fileReader.onload = () => resolve()));
-  return fileReader.result;
-};
